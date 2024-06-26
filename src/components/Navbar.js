dc7de3cf57013dfa3843d565555749ee26c0a22e
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2024-06-26 16:38:11 +0600

    Style added to Navlink inside the JSX

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,10 +2,22 @@
 import { NavLink } from "react-router-dom"
 
 export const Navbar = () => {
+
+    const navLinkStyles = ({ isActive }) => {
+        return {
+            fontWeight: isActive ? 'bold' : 'normal',
+            textDecoration: isActive ? 'none' : 'underline',
+        }
+    }
+
     return (
         <nav>            
-            <NavLink to='/'>Home</NavLink>
-            <NavLink to='/about'>About</NavLink>
+            <NavLink style={navLinkStyles} to='/'>
+                Home
+            </NavLink>
+            <NavLink style={navLinkStyles} to='/about'>
+                About
+            </NavLink>
         </nav>
     )
 }
