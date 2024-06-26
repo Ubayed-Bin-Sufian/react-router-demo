2247b004592ee1d5aceba736ba414fd36788135e
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2024-06-27 01:16:16 +0600

    products route added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { About } from "./components/About";
 import { Navbar } from "./components/Navbar";
 import { OrderSummary } from "./components/OrderSummary";
 import { NoMatch } from "./components/NoMatch";
+import { Products } from "./components/Products";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/order-summary' element={<OrderSummary />} />
+        <Route path='/products' element={<Products />} />
         <Route path='*' element={<NoMatch />}/>
       </Routes>
     </>
