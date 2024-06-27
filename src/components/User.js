88eb3eeccf0cd0716ec217616465314a1697f3a4
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2024-06-28 00:14:56 +0600

    useSearchParasms hook imported, button added to filter

--- src/components/User.js
+++ src/components/User.js
@@ -1,6 +1,11 @@
-import { Outlet } from "react-router-dom"
+// useSearchParams is used to filter out different objects in webpages
+import { Outlet, useSearchParams } from "react-router-dom"
 
 export const Users = () => {
+
+    const [searchParams, setSearchParams] = useSearchParams();
+    const showActiveUsers = searchParams.get('filter') === 'active'
+
     return (
         <>
             <div>
@@ -9,6 +14,17 @@ export const Users = () => {
                 <h2>User 3</h2>
             </div>
             <Outlet />
+            <div>
+                <button onClick={() => setSearchParams({ filter: 'active'})}>Active Users</button>
+                <button onClick={() => setSearchParams({})}>Reset Filters</button>
+            </div>
+            {
+                showActiveUsers ? (
+                    <h2>Show active users</h2>
+                ) : (
+                    <h2>Showing all users</h2>
+                )
+            }
         </>
     )
 }
