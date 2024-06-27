ae510287c4c72d1caf4c2f166036cb778fb28cf1
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2024-06-28 00:04:40 +0600

    useSearchParams hook imported and linked with button

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,19 +1,24 @@
-import { useEffect } from "react";
-import { useParams } from "react-router-dom"
+// import { useEffect } from "react";
+import { useParams, useSearchParams } from "react-router-dom"
 
 export const UserDetails = () => {
+    const [searchParams, setSearchParams] = useSearchParams();
     // const params = useParams()
     // const userID = params.userID
     const {userID} = useParams();
     console.log(userID)
     
-    useEffect(() => {
-        // Perform data fetching based on productId
-      }, [userID]);
+    // useEffect(() => {
+    //     // Perform data fetching based on productId
+    //   }, [userID]);
 
     return (
-        <div>
-            Details about users {userID}
-        </div>
+        <>
+            <div>
+                Details about users {userID}
+            </div>
+            <button onClick={() => setSearchParams({ filter: 'active'})}>Active Users</button>
+            <button onClick={() => setSearchParams({})}>Reset Filters</button>
+        </>
     )
 }
