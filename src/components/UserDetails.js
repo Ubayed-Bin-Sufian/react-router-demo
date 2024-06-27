04f9aacdb3e6b3554e65705716b3e7a1e8591096
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2024-06-28 00:14:13 +0600

    Mistakenly added useSearchParams and buttons

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,8 +1,7 @@
 // import { useEffect } from "react";
-import { useParams, useSearchParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
 
 export const UserDetails = () => {
-    const [searchParams, setSearchParams] = useSearchParams();
     // const params = useParams()
     // const userID = params.userID
     const {userID} = useParams();
@@ -12,13 +11,9 @@ export const UserDetails = () => {
     //     // Perform data fetching based on productId
     //   }, [userID]);
 
-    return (
-        <>
-            <div>
-                Details about users {userID}
-            </div>
-            <button onClick={() => setSearchParams({ filter: 'active'})}>Active Users</button>
-            <button onClick={() => setSearchParams({})}>Reset Filters</button>
-        </>
+    return (      
+        <div>
+            Details about users {userID}
+        </div>        
     )
 }
