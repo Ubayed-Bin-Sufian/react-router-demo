3d0a73c4cc4831c2b6fac184bfc7cef49d00b791
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2024-06-27 23:56:21 +0600

    useEffect hook imported

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,9 +1,15 @@
+import { useEffect } from "react";
 import { useParams } from "react-router-dom"
 
 export const UserDetails = () => {
     // const params = useParams()
     // const userID = params.userID
-    const {userID} = useParams()
+    const {userID} = useParams();
+    console.log(userID)
+    
+    useEffect(() => {
+        // Perform data fetching based on productId
+      }, [userID]);
 
     return (
         <div>
