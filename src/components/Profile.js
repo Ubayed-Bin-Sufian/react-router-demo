c46ec40bbd426c8f6f68a2d7571a7a75e87630cf
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2024-06-30 20:37:01 +0600

    Display logged in username in the profile and add log out button

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,25 @@
+import { useNavigate } from 'react-router-dom'
+import { useAuth } from './auth'
+
 export const Profile = () => {
+
+    // to display logged in username in the profile, useAuth is used
+    const auth = useAuth()
+    const navigate = useNavigate()
+
+    const hangleLogout = () => {
+        auth.logout()
+
+        // after logout, redirect the user to Home Page
+        navigate('/')
+    }
+
     return (
         <div>
-            Profile Page       
+            Welcome {auth.user}
+
+            {/* logout button added */}
+            <button onClick={hangleLogout}>Logout</button>
         </div>
     )
 }
