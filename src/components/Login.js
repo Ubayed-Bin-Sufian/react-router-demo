26b9241007bae110b590804363487e4012b0dcce
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2024-07-01 10:23:30 +0600

    useLocation hook added

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,19 @@
 import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useLocation } from 'react-router-dom'
 import { useAuth } from './auth'
 
 export const Login = () => {
     const [user, setUser] = useState('')
     const auth = useAuth()
     const navigate = useNavigate()
+    const location = useLocation()
+
+    const redirectPath = location.state?.path || '/'
 
     // define a function which call AuthContext
     const handleLogin = () => {
         auth.login(user)
-        navigate('/', { replace: true })
+        navigate(redirectPath, { replace: true })
     }
 
     return (
