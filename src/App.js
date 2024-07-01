de61256a5179db22ea24b8e19f8e4c6fae2d711d
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2024-07-01 10:03:18 +0600

    RequireAuth component imported and routed. Its wrapped around the profile page.

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import { Admin } from "./components/Admin";
 import { Profile } from "./components/Profile";
 import { AuthProvider } from "./components/auth";
 import { Login } from "./components/Login";
+import { RequireAuth } from "./components/RequireAuth";
 
 const LazyAbout = React.lazy(() => import('./components/About'))
 
@@ -37,7 +38,10 @@ function App() {
           <Route path=':userID' element={<UserDetails />} />
           <Route path='admin' element={<Admin />} />
         </Route>
-        <Route path='profile' element={<Profile />} />
+        <Route path='profile' element={
+            <RequireAuth>
+              <Profile />
+            </RequireAuth>} />
         <Route path='login' element={<Login />} />      
         <Route path='*' element={<NoMatch />}/>        
       </Routes>
